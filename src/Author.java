49da83f564258dd07820578ef1da9eb837ffa6c3
Elena <elenela@yandex.ru>
2023-05-14 19:13:19 +0300

    Проверьте, пожалуйста, домашнюю работу № 13!

--- src/Author.java
+++ src/Author.java
@@ -2,5 +2,39 @@ import java.util.Objects;
 public class Author {
 
 
+    private String name;
+    private String  surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname =  surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String toString() {
+        return " Имя:  " + this.name +   "/  Фамилия:  " +  this.surname;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return Objects.equals(getName(), author.getName()) && Objects.equals(getSurname(), author.getSurname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname());
+    }
+
 }
 
